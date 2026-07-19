94871b512873ba733976239da66d51df89df6350
HarikaRanga <165770200+HarikaRanga@users.noreply.github.com>
2026-07-20 01:02:15 +0530

    Add files via upload

---
+++ Module 7 -  Spring REST using Spring Boot 3/spring-learn/src/main/java/com/example/demo/controller/CountryController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Country;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = (Country) context.getBean("country");
+
+        return country;
+    }
+}
